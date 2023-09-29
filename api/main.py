39a5a25b9d97cd4e3c77de5745e6db9b17c4da92
Olivia <none@digitaldebrief.com>
2023-09-29 04:06:19 +0000

    Add login.

--- api/main.py
+++ api/main.py
@@ -1,18 +1,71 @@
 import base64, json
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
 
-import flask
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, session, url_for
 
-app = flask.Flask(__name__)
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
 
-@app.route('/')
-def help():
-    return 'Get a user by going to /user/<username>'
+app = Flask(__name__)
+app.secret_key = env.get("APP_SECRET_KEY") or os.urandom(24)
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
+
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://" + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("home", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
+
+@app.route("/")
+def home():
+    if session and session.get("user"):
+        pretty=json.dumps(session.get('user'), indent=4)
+    else:
+        pretty="Please log in at <a href='/login'>/login</a>"
+    return pretty
+    # return 'Get a user by going to /user/<username>'
 
 
 @app.route('/user/<username>')
 def user(username):
     return json.dumps({'username': username,
-                       'profile_pic': '/image/simly_face.png',
+                       'profile_pic': '/image/smily_face.png',
                        'background_pic': '/image/trees.png',
                        'bio': 'I like the color green and long walks on the beach. My favorite food is pizza. I have a dog named Spot.'})
 
