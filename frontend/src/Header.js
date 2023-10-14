e8a294b9af18d59852f74c09d25e9683594b56e6
Olivia <none@digitaldebrief.com>
2023-10-14 21:13:41 +0000

    Add MUI.

---
+++ frontend/src/Header.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+
+function ElevationScroll( { children }) {
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0
+    });
+  
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+  }
+
+function Header() {
+    return (
+    <React.Fragment>
+      <CssBaseline />
+      <ElevationScroll>
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h6" component="div">
+              Scroll to elevate App bar
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+    </React.Fragment>
+    );
+}
+
+export default Header;
