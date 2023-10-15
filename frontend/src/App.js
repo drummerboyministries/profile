a96c0b2bb4db255007fd4b80bcdf99f0504dee85
Olivia <none@digitaldebrief.com>
2023-10-15 00:37:42 +0000

    Add profile to AppBar.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,15 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import Header from './Header';
+
 import { Container, Toolbar } from '@mui/material';
+
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <Header></Header>
-      </header>
+      <Header></Header>
       <Toolbar />
       <Container>
         <p>
--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,27 +1,47 @@
-import * as React from 'react';
+import { cloneElement, MouseEvent, useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 
-function ElevationScroll( { children }) {
-    // Note that you normally won't need to set the window ref as useScrollTrigger
-    // will default to window.
-    // This is only being set here because the demo is in an iframe.
-    const trigger = useScrollTrigger({
-      disableHysteresis: true,
-      threshold: 0
-    });
-  
-    return React.cloneElement(children, {
-      elevation: trigger ? 4 : 0,
-    });
-  }
+
+function ElevationScroll({ children }) {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0
+  });
+
+  return cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
 
 function Header() {
-    return (
-    <React.Fragment>
+
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const settings = ['Profile', 'Account', 'Logout'];
+  return (
+    <header>
       <CssBaseline />
       <ElevationScroll>
         <AppBar>
@@ -29,11 +49,41 @@ function Header() {
             <Typography variant="h6" component="div">
               Scroll to elevate App bar
             </Typography>
+
+            <Box sx={{ flex: 1, textAlign: 'right'}}>
+              <Tooltip title="Open user settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Me me me" src="/smily_face.png" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
-    </React.Fragment>
-    );
+    </header>
+  );
 }
 
 export default Header;
